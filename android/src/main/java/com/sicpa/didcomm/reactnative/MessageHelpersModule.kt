3c7eef810e3296ace770c0c826b548fbf0050608
alexander.shenshin <alexander.shenshin@dsr-corporation.com>
2022-05-13 00:31:22 +0300

    Update native module data transfer

--- android/src/main/java/com/sicpa/didcomm/reactnative/MessageHelpersModule.kt
+++ android/src/main/java/com/sicpa/didcomm/reactnative/MessageHelpersModule.kt
@@ -1,31 +1,29 @@
 package com.sicpa.didcomm.reactnative
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.*
 import org.didcommx.didcomm.DIDComm
 import org.didcommx.didcomm.diddoc.DIDDocResolver
 import org.didcommx.didcomm.message.Message
 import org.didcommx.didcomm.model.PackEncryptedParams
-import org.didcommx.didcomm.model.PackEncryptedResult
 import org.didcommx.didcomm.model.UnpackParams
-import org.didcommx.didcomm.model.UnpackResult
 import org.didcommx.didcomm.secret.SecretResolver
+import org.json.JSONObject
 
 class MessageHelpersModule(context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
     override fun getName() = "DIDCommMessageHelpers"
 
     @ReactMethod
     fun pack_encrypted(
-        message: Message,
+        messageData: ReadableMap,
         to: String,
         from: String? = null,
         signFrom: String? = null,
         protectSender: Boolean = true,
         didDocResolver: DIDDocResolver,
         secretsResolver: SecretResolver
-    ): PackEncryptedResult {
+    ): WritableMap {
         val didComm = DIDComm(didDocResolver, secretsResolver)
+        val message = Message.parse(messageData.toHashMap())
 
         var builder = PackEncryptedParams
             .builder(message, to)
@@ -34,16 +32,16 @@ class MessageHelpersModule(context: ReactApplicationContext) : ReactContextBaseJ
         builder = from?.let { builder.from(it) } ?: builder
         builder = signFrom?.let { builder.signFrom(it) } ?: builder
 
-        val params = builder.build()
-        return didComm.packEncrypted(params)
+        val packResult = didComm.packEncrypted(builder.build())
+        return ReactNativeJsonUtils.convertJsonToMap(JSONObject(packResult))
     }
 
     @ReactMethod
-    fun unpack(packedMessage: String,didDocResolver: DIDDocResolver, secretsResolver: SecretResolver): UnpackResult {
+    fun unpack(packedMessage: String, didDocResolver: DIDDocResolver, secretsResolver: SecretResolver): WritableMap {
         val didComm = DIDComm(didDocResolver, secretsResolver)
 
-        val result = didComm.unpack(UnpackParams.Builder(packedMessage).build())
+        val unpackResult = didComm.unpack(UnpackParams.Builder(packedMessage).build())
 
-        return UnpackResult(result.message, result.metadata)
+        return ReactNativeJsonUtils.convertJsonToMap(JSONObject(unpackResult))
     }
 }
---
+++ android/src/main/java/com/sicpa/didcomm/reactnative/ReactNativeJsonUtils.kt
@@ -0,0 +1,115 @@
+package com.sicpa.didcomm.reactnative
+
+import com.facebook.react.bridge.*
+import org.json.JSONArray
+import org.json.JSONObject
+
+//Based on https://gist.github.com/viperwarp/2beb6bbefcc268dee7ad
+object ReactNativeJsonUtils {
+    fun convertJsonToMap(jsonObject: JSONObject): WritableMap {
+        val map: WritableMap = WritableNativeMap()
+
+        val keysIterator = jsonObject.keys()
+        for(key in keysIterator) {
+            when (val value = jsonObject[key]) {
+                is JSONObject -> {
+                    map.putMap(key, convertJsonToMap(value))
+                }
+                is JSONArray -> {
+                    map.putArray(key, convertJsonToArray(value))
+                }
+                is Boolean -> {
+                    map.putBoolean(key, value)
+                }
+                is Int -> {
+                    map.putInt(key, value)
+                }
+                is Double -> {
+                    map.putDouble(key, value)
+                }
+                is String -> {
+                    map.putString(key, value)
+                }
+                else -> {
+                    map.putString(key, value.toString())
+                }
+            }
+        }
+
+        return map
+    }
+
+    fun convertJsonToArray(jsonArray: JSONArray): WritableArray {
+        val array: WritableArray = WritableNativeArray()
+
+        for (element in jsonArray) {
+            when (element) {
+                is JSONObject -> {
+                    array.pushMap(convertJsonToMap(element))
+                }
+                is JSONArray -> {
+                    array.pushArray(convertJsonToArray(element))
+                }
+                is Boolean -> {
+                    array.pushBoolean(element)
+                }
+                is Int -> {
+                    array.pushInt(element)
+                }
+                is Double -> {
+                    array.pushDouble(element)
+                }
+                is String -> {
+                    array.pushString(element)
+                }
+                else -> {
+                    array.pushString(element.toString())
+                }
+            }
+        }
+
+        return array
+    }
+
+    fun convertMapToJson(readableMap: ReadableMap?): JSONObject {
+        val jsonObject = JSONObject()
+
+        val iterator = readableMap?.keySetIterator()
+        while (iterator?.hasNextKey() == true) {
+            val key = iterator.nextKey()
+            val value = when (readableMap.getType(key)) {
+                ReadableType.Null -> JSONObject.NULL
+                ReadableType.Boolean -> readableMap.getBoolean(key)
+                ReadableType.Number -> readableMap.getDouble(key)
+                ReadableType.String -> readableMap.getString(key)
+                ReadableType.Map -> convertMapToJson(readableMap.getMap(key))
+                ReadableType.Array -> convertArrayToJson(readableMap.getArray(key))
+            }
+            jsonObject.put(key, value)
+        }
+
+        return jsonObject
+    }
+
+    fun convertArrayToJson(readableArray: ReadableArray?): JSONArray {
+        val array = JSONArray()
+
+        if(readableArray == null) {
+            return array
+        }
+
+        for (index in 0 until (readableArray.size())) {
+            val element = when (readableArray.getType(index)) {
+                ReadableType.Null -> JSONObject.NULL
+                ReadableType.Boolean -> readableArray.getBoolean(index)
+                ReadableType.Number -> readableArray.getDouble(index)
+                ReadableType.String -> readableArray.getString(index)
+                ReadableType.Map -> convertMapToJson(readableArray.getMap(index))
+                ReadableType.Array -> convertArrayToJson(readableArray.getArray(index))
+            }
+            array.put(element)
+        }
+
+        return array
+    }
+}
