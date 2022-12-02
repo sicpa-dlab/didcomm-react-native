c2d1eb302d1b64c2d7d2145b3cb0a45aa4114ec1
Sacha Kozma <sacha.kozma@sicpa.com>
2022-12-02 12:55:58 +0100

    Switch imports from sicpa-dlab to sicpa_open_source

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,4 +1,4 @@
-import { DIDCommResolversProxy } from "@sicpa-dlab/didcomm-react-native"
+import { DIDCommResolversProxy } from "@sicpa_open_source/didcomm-react-native"
 import * as React from "react"
 import { useEffect } from "react"
 import { StyleSheet, View, Text, Button, NativeEventEmitter, NativeModules } from "react-native"
